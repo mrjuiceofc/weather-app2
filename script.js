b24eda1e726b10cae468f8725430856bee35b440
Oktay Shakirov <oktayshakirov@gmail.com>
2023-06-22 09:50:12 +0200

    update
    - removed second digit after the dot

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ let weather = {
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".temp").innerText = temp.toFixed(1) + "°C";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText =
       "Wind speed: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?landscape/?" + name + "')";
+      "url('https://source.unsplash.com/1600x900/?nature/?" + name + "')";
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
